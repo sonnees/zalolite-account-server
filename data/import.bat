4c1b2134e3630fa168a5b669e1f4b2af38a8a924
sonnees <sson12131415@gmail.com>
2024-05-03 10:41:15 +0700

    init

---
+++ data/import.bat
@@ -0,0 +1,4 @@
+mongoimport --db AccountService --collection account --file accounts.json
+mongoimport --db ChatService --collection user --file users.json
+mongoimport --db ChatService --collection chat --file chats.json
+mongoimport --db ChatService --collection group --file groups.json
