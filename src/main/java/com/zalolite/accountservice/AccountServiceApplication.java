df277f66acde5826f74b2eee9a78a123f1453496
sonnees <sson12131415@gmail.com>
2024-05-04 09:19:39 +0700

    fix connect eureka EnableDiscoveryClient

--- src/main/java/com/zalolite/accountservice/AccountServiceApplication.java
+++ src/main/java/com/zalolite/accountservice/AccountServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
 @EnableReactiveMongoRepositories
+@EnableDiscoveryClient
 public class AccountServiceApplication implements CommandLineRunner{
 
     @Autowired
